9d0ac5c508e390c7dd8a2a4931d2eea7aa60d9f1
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-05-18 10:00:59 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -26,12 +26,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*s;
 	int		i;
 
-/*	if (s1 == NULL)
-	{
-		s1 = (char *)malloc(1 * sizeof(char));
-		s1[0] = '\0';
-	}
-*/	s = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	s = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!s || !s1 || !s2)
 		return (NULL);
 	i = -1;
@@ -40,8 +35,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	i = -1;
 	while (s2[++i] != '\0')
 		s[ft_strlen(s1) + i] = s2[i];
-	s[ft_strlen(s1) + i] = '\0';
-//	free(s1);
+	s[ft_strlen(s1) + ft_strlen(s2)  + i] = '\0';
 	return (s);
 }
 
