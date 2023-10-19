adec5e2e93f8149201464e60d109bacbbf46652b
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-10-19 22:34:46 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -21,24 +21,6 @@ unsigned long int	ft_strlen(char *s)
 	return (i);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*s;
-	int		i;
-
-	s = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!s || !s1 || !s2)
-		return (NULL);
-	i = -1;
-	while (s1[++i] != '\0')
-		s[i] = s1[i];
-	i = -1;
-	while (s2[++i] != '\0')
-		s[ft_strlen(s1) + i] = s2[i];
-	s[ft_strlen(s1) + i] = '\0';
-	return (s);
-}
-
 char	*ft_strchr(char *save, int character)
 {
 	unsigned long int	i;
@@ -71,4 +53,22 @@ void	*ft_calloc(unsigned long int size, unsigned long int c)
 		return (NULL);
 	ft_bzero(result, (size * c));
 	return (result);
+}
+
+char	*ft_strcat(char *s1, char *s2)
+{
+	char	*s;
+	int		i;
+
+	s = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!s || !s1 || !s2)
+		return (NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		s[i] = s1[i];
+	i = -1;
+	while (s2[++i] != '\0')
+		s[ft_strlen(s1) + i] = s2[i];
+	s[ft_strlen(s1) + i] = '\0';
+	return (s);
 }
