79ca8b8e6f89e1e120ffc246d59a21ebc836aaa4
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2024-02-07 12:58:34 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,9 +13,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
